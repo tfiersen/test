1b8d986d97ba16e6d9faa46876b6c9a986f97e1d
tfiersen <timo.fiersen@web.de>
2013-07-16 22:19:44 +0200

    Re-added

---
+++ Controller/TestAppController.php
@@ -0,0 +1,25 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+/**
+ * Class TestAppController
+ */
+class TestAppController extends AppController {
+
+	/**
+	 * @var int
+	 */
+	public $bar = 42;
+
+	/**
+	 * Returns `bar`
+	 *
+	 * @return string
+	 */
+	public function foo()
+	{
+		return 'bar';
+	}
+
+}
