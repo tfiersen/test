bc93417f8d2754436e54decb700aafccc31793a5
tfiersen <timo.fiersen@web.de>
2013-07-16 17:07:44 +0200

    Add some method

--- Controller/TestAppController.php
+++ Controller/TestAppController.php
@@ -4,4 +4,14 @@ App::uses('AppController', 'Controller');
 
 class TestAppController extends AppController {
 
+	/**
+	 * Returns `bar`
+	 *
+	 * @return string
+	 */
+	public function foo()
+	{
+		return 'bar';
+	}
+
 }
