9c4028058975b9d77da9f3759b3a795e6913d25a
tfiersen <timo.fiersen@web.de>
2013-10-12 13:18:28 +0200

    test

--- Model/TestAppModel.php
+++ Model/TestAppModel.php
@@ -4,4 +4,16 @@ App::uses('AppModel', 'Model');
 
 class TestAppModel extends AppModel {
 
+	public function beforeSave($options = array())
+	{
+		if(!parent::beforeSave($options))
+		{
+			return false;
+		}
+
+		// ...
+
+		return true;
+	}
+
 }
